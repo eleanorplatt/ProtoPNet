014ce4a95d07472f313f45a6a5af99023a054f1c
eleanorplatt <elplatt@hotmail.co.uk>
2021-01-05 16:41:36 +0000

    fixing device problems

--- main.py
+++ main.py
@@ -123,19 +123,19 @@ joint_optimizer_specs = \
  {'params': ppnet.add_on_layers.parameters(), 'lr': joint_optimizer_lrs['add_on_layers'], 'weight_decay': 1e-3},
  {'params': ppnet.prototype_vectors, 'lr': joint_optimizer_lrs['prototype_vectors']},
 ]
-joint_optimizer = torch.optim.Adam(joint_optimizer_specs).to(device)
-joint_lr_scheduler = torch.optim.lr_scheduler.StepLR(joint_optimizer, step_size=joint_lr_step_size, gamma=0.1).to(device)
+joint_optimizer = torch.optim.Adam(joint_optimizer_specs)
+joint_lr_scheduler = torch.optim.lr_scheduler.StepLR(joint_optimizer, step_size=joint_lr_step_size, gamma=0.1)
 
 from settings import warm_optimizer_lrs
 warm_optimizer_specs = \
 [{'params': ppnet.add_on_layers.parameters(), 'lr': warm_optimizer_lrs['add_on_layers'], 'weight_decay': 1e-3},
  {'params': ppnet.prototype_vectors, 'lr': warm_optimizer_lrs['prototype_vectors']},
 ]
-warm_optimizer = torch.optim.Adam(warm_optimizer_specs).to(device)
+warm_optimizer = torch.optim.Adam(warm_optimizer_specs)
 
 from settings import last_layer_optimizer_lr
 last_layer_optimizer_specs = [{'params': ppnet.last_layer.parameters(), 'lr': last_layer_optimizer_lr}]
-last_layer_optimizer = torch.optim.Adam(last_layer_optimizer_specs).to(device)
+last_layer_optimizer = torch.optim.Adam(last_layer_optimizer_specs)
 
 # weighting of different training losses
 from settings import coefs
